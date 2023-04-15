bb0445ed9170fe8f2be4d16d6c2cb2c3b14e3318
Luca Tabone <taboneluca3@gmail.com>
2023-04-15 12:07:51 +0200

    Add <PageLoader /> Unit Tests

---
+++ src/components/__tests__/PageLoader.spec.js
@@ -0,0 +1,40 @@
+import { render, screen } from '@testing-library/vue'
+import { describe, it, expect, beforeEach, afterEach } from 'vitest'
+
+import PageLoader from '../PageLoader.vue'
+
+describe('<PageLoader />', () => {
+  let info = null
+
+  afterEach(() => {
+    info.unmount()
+  })
+
+  describe('Rendering a <PageLoader /> without a message', () => {
+    describe('When rendering a <PageLoader /> without a message', () => {
+      beforeEach(() => {
+        info = render(PageLoader)
+      })
+
+      it('should render a <PageLoader /> with a default message', () => {
+        expect(screen.getByText("We'll be right with you!")).not.toBeUndefined()
+      })
+    })
+  })
+
+  describe('Rendering a <PageLoader /> with a message', () => {
+    describe('When rendering a <PageLoader /> with a message', () => {
+      beforeEach(() => {
+        info = render(PageLoader, {
+          props: {
+            message: 'test-message'
+          }
+        })
+      })
+
+      it('should render a <PageLoader /> with the specified message', () => {
+        expect(screen.getByText('test-message')).not.toBeUndefined()
+      })
+    })
+  })
+})
